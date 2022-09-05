40172a84935d934b2b8c55a66d2706b7b5b7f87d
haraphat01 <haraphat@gmail.com>
2022-09-05 09:45:38 +0100

    working on the base components

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Nav = () => {
+    return ( <div>
+        <nav className="nabar">
+            <h1 className="">
+                The Blog
+            </h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Create Blog</a>
+            </div>
+        </nav>
+    </div> );
+}
+ 
+export default Nav;
