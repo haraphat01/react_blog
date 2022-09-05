4016ffd98e8c9f86a7d2b336961a4990deb1a718
haraphat01 <haraphat@gmail.com>
2022-09-05 14:41:47 +0100

    created the base css

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: "My Website", body: "Welcome here wereu", author: "john", id: 1},
+        {title: "My Blog", body: "Welcome here wereu", author: "john", id: 1},
+        {title: "My Websites", body: "Welcome here wereu", author: "john", id: 1}
+    ])
     return (
         <div className="home">
             <h2>Homepage</h2>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" onKeyUpCapture={(blog.id)}>
+                    <h2>{blog.author}</h2>
+                    <p> Written by {blog.title}</p>
+                </div>
+            ))}
         </div>
       );
 }
